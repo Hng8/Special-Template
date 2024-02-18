320b9ce1a670b20f41f155a22c9a59030927a723
Hng8 <hamed20161380@gmail.com>
2024-02-18 11:15:47 -0600

    Our gallery section added

--- js/master.js
+++ js/master.js
@@ -24,21 +24,6 @@ if (mainColors !== null) {
         }
     });
 }
-// Check if there's a preference stored in local storage
-// let isRandomBg = localStorage.getItem("randomBackground");
-
-// // Get the checkbox element
-// let checkbox = document.getElementById("checkbox1");
-
-// // If there's a preference, update the checkbox state accordingly
-// if (isRandomBg !== true) {
-//     checkbox.checked = (isRandomBg === "true"); // Convert string to boolean
-//     if (checkbox.checked) {
-//         changeBg(); // If random background is preferred, call changeBg to start changing the background randomly
-//     }
-// } else {
-//     checkbox.checked = false;
-// }
 
 // Select gear variable and Show setting box
 gear.addEventListener("click", () => {
@@ -156,3 +141,80 @@ window.onscroll = function () {
         });
     }
 };
+
+// Create Popup With The Image
+let ourGallery = document.querySelectorAll(".gallery img");
+
+ourGallery.forEach(img => {
+
+    img.addEventListener('click', (e) => {
+
+        // Create Overlay Element
+        let overlay = document.createElement('div');
+
+        // Add Class To Overlay
+        overlay.className = 'popup-overlay';
+
+        // Append Overlay To The Body
+        document.body.appendChild(overlay);
+
+        // Create popup 
+        let popupBox = document.createElement("div");
+
+        // Add Class To The Popup Box
+        popupBox.className = 'popup-box';
+
+        // img heading 
+        if (img.alt !== null) {
+
+            // Create Heading
+            let imgHeading = document.createElement("h3");
+
+            // Create Text for heading
+            let imgText = document.createTextNode(img.alt);
+
+            // Append The Text To The Heading
+            imgHeading.appendChild(imgText);
+
+            // Append The Heading To The Popup Box
+            popupBox.appendChild(imgHeading);
+        }
+
+        // Create The image
+        let popupImage = document.createElement("img");
+
+        // Set Image Sourse
+        popupImage.src = img.src;
+
+        // Add Image To Popup Box
+        popupBox.appendChild(popupImage);
+
+        // Append THe popup Box To Body
+        document.body.appendChild(popupBox);
+
+        // Create Clsoing span
+        let closeSpan = document.createElement("span");
+
+        closeSpan.className = "close-button"
+
+        let closeSpanText = document.createTextNode("X");
+
+        closeSpan.appendChild(closeSpanText);
+
+        popupBox.appendChild(closeSpan);
+
+    })
+})
+
+// Close popup 
+document.addEventListener("click", (e) => {
+
+    if (e.target.className == 'close-button') {
+
+        // Remove the current popup
+        e.target.parentElement.remove();
+
+        // Remove Overlay 
+        document.querySelector(".popup-overlay").remove();
+    }
+})
